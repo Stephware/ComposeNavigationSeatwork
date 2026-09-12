1e448ff1de2be50e4107a3dfeb02057c962da447
Stephware <pinacate.stephen@gmail.com>
2026-09-12 11:22:25 +0800

    reverted back to mutablestateof, android studio showed warning so i changed it earlier but instruction said otherwise

--- app/src/main/java/com/example/composenavigationseatwork/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/composenavigationseatwork/screens/home/HomeScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -47,7 +46,7 @@ fun HomeScreen(
     }
 
     var loginAttempts by remember {
-        mutableIntStateOf(0)
+        mutableStateOf(0)
     }
 
     var errorMessage by remember {
